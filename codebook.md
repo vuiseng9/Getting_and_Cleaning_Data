a4f6b782d98413b42a608ad2732b71b66413db5c
vuiseng9 <vuiseng9@gmail.com>
2016-09-09 01:14:28 +0800

    first commit

---
+++ codebook.md
@@ -0,0 +1,8 @@
+## Column Dictionary of df_mean.txt
+
+This data set is a processed output of data from
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+Within the downloaded file above, it contains the initial data description and naming convention, which are the prerequisite to this document.
+
+Each column in df_mean only contains numeric/foating number, which is mean of a feature per type of activity.
---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+# run_analysis.R
+# go to working directory
+# setwd('C:/Users/vchua/Desktop/MyProCert/Getting_and_Cleaning_Data/project/')
+
+library(dplyr)
+
+# Import train feature matrix
+X_train <- read.table('UCI HAR Dataset/train/X_train.txt',header = FALSE)
+
+# Import test feature matrix
+X_test <- read.table('UCI HAR Dataset/test/X_test.txt',header = FALSE)
+
+# Concatenate train and test feature matrix
+X <- rbind(X_train,X_test)
+
+# Import feature names
+features <- read.table('UCI HAR Dataset/features.txt',header=FALSE)
+
+# renaming the column names with the last column of data frame "features'
+# getting rid of '()"
+names(X) <- gsub("\\(\\)","",as.character(features[[-1]]))
+
+# Import train label
+y_train <- read.table('UCI HAR Dataset/train/y_train.txt',header = FALSE)
+
+# Import test label
+y_test <- read.table('UCI HAR Dataset/test/y_test.txt',header = FALSE)
+
+# Concatenate train and test train label
+y <- rbind(y_train,y_test)
+
+# Renaming the col as activity_id since they are integer values
+names(y) <- "activity_id"
+
+# Import lookup table of integer to activity
+label <- read.table('UCI HAR Dataset/activity_labels.txt',header = FALSE)
+
+# Renaming the columns of the lookup dataframe
+names(label) <- c("activity_id","activity")
+
+# left join on y with label, output is a dataframe with id and activity
+# Do avoid of sorting to keep the label in the right order to the feature matrix
+y <- merge(y,label,by = "activity_id",all.x = TRUE, sort = FALSE)
+
+# Bind feature matrix to label to form a master dataframe
+df <- cbind(X,y)
+
+# Subsetting to only mean and std columns
+subset_df <- df[grep("mean|std",names(df),ignore.case = TRUE)]
+subset_df$activity <- df$activity
+
+# dataset of feature mean per activity
+df_mean <- 
+  subset_df %>% 
+  group_by(activity) %>% 
+  summarise_each(funs(mean))
